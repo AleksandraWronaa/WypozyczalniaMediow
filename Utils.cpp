7759dd58f513f4af6b6cc1afbb08ac20bb8b3fa4
Aleksandra <149684219+AleksandraWronaa@users.noreply.github.com>
2026-01-09 15:45:17 +0100

    Delete Utils.cpp

--- Utils.cpp
+++
@@ -1,8 +0,0 @@
-#include "Utils.h"
-
-std::string dataNaString(time_t czas) {
-    char buffer[80];
-    struct tm* timeinfo = localtime(&czas);
-    strftime(buffer, 80, "%Y-%m-%d %H:%M:%S", timeinfo);
-    return std::string(buffer);
-}
